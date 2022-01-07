06c9ae91e06da57122a4bf34e77654704e829046
Elena Sysoyeva <evsysoyeva@gmail.com>
2022-01-07 18:48:13 +0200

    test3

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 
-axios.defaults.baseURL = 'https://lpj-tasker.herokuapp.com';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 const token = {
   set(token) {
